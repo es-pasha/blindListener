a27a1c2d5caeb63ab94787931f34e9816411035f
vanyasirotyuk <vanya@eventsoft.com.ua>
2013-12-24 19:15:33 +0200

    usb hoock

--- BlindLibrary/Program.cs
+++ BlindLibrary/Program.cs
@@ -2,15 +2,26 @@ using System;
 using System.IO;
 using System.Media;
 using System.Threading;
+using LibUsbDotNet;
+using LibUsbDotNet.Main;
+using LibUsbDotNet.DeviceNotify;
 
 namespace BlindLibrary
 {
 	class MainClass
 	{
 		public static Menu mainMenu = new Menu ();
-
+		public static UsbDeviceFinder MyUsbFinder;
+		public static IDeviceNotifier UsbDeviceNotifier;
+		
 		public static void Main (string[] args)
 		{
+			//Find your vendor id etc by listing all available USB devices
+			MyUsbFinder = new UsbDeviceFinder(0x2341, 0x0001);
+			UsbDeviceNotifier = DeviceNotifier.OpenDeviceNotifier();
+			
+			
+			/*
 			Menu menu = new Menu();
 			menu.Init("input.txt");		
 			while(true)
@@ -28,6 +39,28 @@ namespace BlindLibrary
 				}
 				menu.Clear();
 			}
+			*/
+			Console.ReadKey();
+		}
+	
+		public UsbDevice MyUsbDevice;
+
+
+		private void OnDeviceNotifyEvent(object sender, DeviceNotifyEventArgs e)
+		{
+		    if (e.Object.ToString().Split('\n')[1].Contains("0x2341"))
+		    {
+		        if (e.EventType == EventType.DeviceArrival)
+		        {
+		            //Connect();
+					Console.WriteLine("Connect!");
+		        }
+		        else if(e.EventType == EventType.DeviceRemoveComplete)
+		        {
+		            //ResetConnection();
+					Console.WriteLine("Disconnect!");
+		        }
+		    }
 		}
 	}
 }
